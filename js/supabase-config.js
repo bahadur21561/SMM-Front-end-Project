8ae75181a5acd9c3e6f81b484698e165015ef20b
bahadur21561 <bahadur21561@gmail.com>
2026-08-11 12:49:02 +0500

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,6 @@
+// Supabase Credentials
+const SUPABASE_URL = 'https://pyfhyewkadxcpvpmagnz.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ZVUK6e371jf21see2pD6Mg_ZbfA_qkR';
+
+// Initialize Client
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
